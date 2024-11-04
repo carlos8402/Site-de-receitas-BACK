66c94035b606f19a8f27664d122f4dd6b40c57a9
carlos8468 <carlos_eduardo8468@hotmail.com>
2024-11-04 18:33:09 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const server = express();
+const cors = require("cors");
+const fs = require("fs");
+
+server.use(cors());
+server.use(express.json());
+
+let recipes = require("./src/data/recipes.json");
+
+// Rota para obter todas as receitas
+server.get("/recipes", (req, res) => {
+  return res.json({ recipes });
+});
+
+// Rota para obter uma receita específica por ID
+server.get("/recipes/:id", (req, res) => {
+  const { id } = req.params;
+  const recipe = recipes.find((r) => r.id === parseInt(id));
+
+  if (!recipe) {
+    return res.status(404).send("Recipe not found");
+  }
+
+  return res.json(recipe);
+});
+
+// Rota para criar uma nova receita
+server.post("/recipes", (req, res) => {
+  const { titulo, descricao, ingredientes, modo_preparo, tipo, foto } =
+    req.body;
+
+  console.log("Dados recebidos:", req.body);
+
+  // Verificar se todos os campos obrigatórios foram preenchidos
+  if (
+    !titulo ||
+    !descricao ||
+    !ingredientes ||
+    !modo_preparo ||
+    !tipo ||
+    !foto
+  ) {
+    return res.status(400).send("Todos os campos são obrigatórios");
+  }
+
+  // Criar a nova receita
+  const newRecipe = {
+    id: recipes.length + 1,
+    titulo,
+    descricao,
+    ingredientes,
+    modo_preparo,
+    tipo,
+    foto,
+  };
+
+  // Adicionar a nova receita ao array de receitas
+  recipes.push(newRecipe);
+
+  // Salvar a nova receita no arquivo JSON
+  fs.writeFile(
+    "./src/data/recipes.json",
+    JSON.stringify(recipes, null, 2),
+    (err) => {
+      if (err) {
+        return res.status(500).send("Erro ao salvar a receita");
+      }
+
+      // Retornar a nova receita criada
+      return res.status(201).json(newRecipe);
+    }
+  );
+});
+
+// Iniciar o servidor
+server.listen(3000, () => {
+  console.log("Servidor está funcionando na porta 3000");
+});
